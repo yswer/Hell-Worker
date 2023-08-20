9962b9aba5379e16322274a4cca7e513cc0a7173
kxxxxyr <kkakyr@163.com>
2023-08-21 02:01:20 +0800

    commit一下

--- Assets/Scripts/BattleScene/BattleManager.cs
+++ Assets/Scripts/BattleScene/BattleManager.cs
@@ -12,7 +12,7 @@ public class BattleManager : MonoBehaviour
     void Start()
     {
         enemySpawnManager = GameObject.Find("Enemy").GetComponent<EnemySpawnManager>();
-        enemySpawnManager.SpawnEnemy();
+        droppedItem.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,7 +21,20 @@ public class BattleManager : MonoBehaviour
         if(enemySpawnManager.numOfSpawn <=0 && enemySpawnManager.enemies == 0) battleEnd = true;
         if(battleEnd)
         {
-
+            Debug.Log("战斗结束");
+            // 显示掉落
+            droppedItem.SetActive(true);
+            // 门可以触发
         }
+        // GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");//返回tag相同的所有物体
+        // foreach(GameObject target in targets)
+        // {
+        //     //下面的Distance函数的距离可以根据需求设置
+        //     if(Vector3.Distance(target.transform.position,gameObject.transform.position)<=1.01f)
+        //     {
+        //     //可以筛选出一定距离内的物体
+        //     //我通常使用这个方法并结合其他方法找到我所需要的特定的游戏物体对象
+        //     }
+        // }
     }
 }
